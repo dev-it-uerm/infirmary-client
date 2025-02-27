bc965653777094e2ca061599895bee91c8123510
JP Pascua <jmpascua@uerm.edu.ph>
2025-02-27 16:26:50 +0800

    Log In page visual bug fix

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -14,8 +14,8 @@
     <div
       class="absolute"
       style="
-        width: 100vw;
-        height: 100vh;
+        width: 100%;
+        height: 100%;
         background-color: rgba(255, 255, 255, 0.5);
       "
     />
